745dd7e3026c382c3ea82ad66aa627ff3e5f440b
saga <sprepteis@gmail.com>
2024-06-16 02:51:59 -0300

    test

---
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureKill.kt
@@ -0,0 +1,73 @@
+package at.hannibal2.skyhanni.features.fishing
+
+import at.hannibal2.skyhanni.events.MobEvent
+import at.hannibal2.skyhanni.events.SeaCreatureFishEvent
+import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.InventoryUtils
+import at.hannibal2.skyhanni.utils.ItemUtils.itemName
+import at.hannibal2.skyhanni.utils.LorenzUtils
+import net.minecraft.client.Minecraft
+import net.minecraft.client.settings.KeyBinding
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
+
+@SkyHanniModule
+object SeaCreatureKill {
+
+    private var seaCreatures = setOf<String>()
+    private var initialPitch = 0f
+
+    @SubscribeEvent
+    fun onSeaCreatureFish(event: SeaCreatureFishEvent) {
+        if (!isEnabled()) return
+
+        val mob = event.seaCreature
+        val creature = SeaCreatureManager.allFishingMobs[mob.name] ?: return
+
+        if (creature.rarity.id > 4) return
+
+        seaCreatures += mob.name
+
+        initialPitch = LorenzUtils.getPlayer()?.cameraPitch ?: return
+        smoothRotate(90f)
+    }
+
+    @SubscribeEvent
+    fun onMobDeSpawn(event: MobEvent.DeSpawn.SkyblockMob) {
+        seaCreatures -= event.mob.name
+
+        smoothRotate(initialPitch)
+    }
+
+    private fun smoothRotate(pitch: Float) {
+        val player = LorenzUtils.getPlayer() ?: return
+
+        val initialPitch = player.cameraPitch
+        val diffPitch = initialPitch - pitch
+
+        var time = 0
+
+        while (time < 5) {
+            val pitchToSet = initialPitch + diffPitch * (0.50 + time * 0.10)
+            player.cameraPitch = pitchToSet.toFloat()
+
+            time += 1
+        }
+    }
+
+    @SubscribeEvent
+    fun onTick(event: TickEvent.ClientTickEvent) {
+        if (!isEnabled() || seaCreatures.isEmpty()) return
+        if (event.phase !== TickEvent.Phase.END) return
+
+        val player = LorenzUtils.getPlayer() ?: return
+
+        val item = InventoryUtils.getItemsInHotbar().indexOfFirst { it.itemName.contains("Hyperion") } ?: return
+        player.inventory.currentItem = item
+
+        KeyBinding.onTick(Minecraft.getMinecraft().gameSettings.keyBindUseItem.keyCode)
+    }
+
+    private fun isEnabled() = LorenzUtils.inSkyBlock && !DungeonAPI.inDungeon() && !LorenzUtils.inKuudraFight
+}
