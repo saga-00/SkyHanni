19d3087eb26ba0f65c8b48d207b558edc14d11df
saga <sprepteis@gmail.com>
2024-06-16 04:05:41 -0300

    polishing time

--- src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureKill.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureKill.kt
@@ -4,7 +4,6 @@ import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.events.SeaCreatureFishEvent
 import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import net.minecraft.client.Minecraft
